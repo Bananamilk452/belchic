d3a88d0f9d2808d9a28dcc3f3fca1460654f3316
boracat <meow@bora.cat>
2026-05-06 22:33:03 +0900

    feat(ui): 모바일 사이드바 기본 디자인

--- components/global-header.tsx
+++ components/global-header.tsx
@@ -1,6 +1,24 @@
-import { HeartIcon, SearchIcon, ShoppingBagIcon, UserIcon } from "lucide-react";
+"use client";
+
+import {
+  HeartIcon,
+  MenuIcon,
+  SearchIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const NAV_MENU: Array<{ label: string; path: string }> = [
   {
@@ -9,21 +27,55 @@ const NAV_MENU: Array<{ label: string; path: string }> = [
   },
   {
     label: "새로 들어온 상품",
-    path: "/",
+    path: "/new",
   },
 ];
 
 export function GlobalHeader() {
+  const pathname = usePathname();
+
   return (
     <div className="w-full flex justify-center items-center bg-white border-b border-border">
       <div className="flex gap-8 justify-between items-center py-5 px-4 md:px-12 w-full max-w-7xl">
-        <div>
-          <span className="text-lg">belchic</span>
+        <div className="flex items-center justify-center gap-2">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon-lg" className="flex md:hidden">
+                <MenuIcon className="size-5" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" showCloseButton={false}>
+              <SheetHeader>
+                <SheetTitle>Belchic</SheetTitle>
+                <SheetDescription />
+              </SheetHeader>
+              <div className="flex-1 flex flex-col">
+                {NAV_MENU.map((item) => (
+                  <Link
+                    key={item.label}
+                    className={cn(
+                      "py-2 px-6 group",
+                      pathname === item.path && "bg-secondary",
+                    )}
+                    href={item.path}
+                  >
+                    <span className="text-lg">{item.label}</span>
+                  </Link>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+          <span className="text-lg">Belchic</span>
         </div>
         <div className="flex-1 hidden md:flex flex-wrap">
           {NAV_MENU.map((item) => (
             <Link key={item.label} className="p-2 group" href={item.path}>
-              <span className="group-hover:border-b border-border">
+              <span
+                className={cn(
+                  "group-hover:border-b border-border",
+                  pathname === item.path && "border-b group-hover:border-black",
+                )}
+              >
                 {item.label}
               </span>
             </Link>
