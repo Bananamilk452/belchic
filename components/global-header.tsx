b39e449c196396f54bf234456def1ebdd1f40926
boracat <meow@bora.cat>
2026-05-07 15:21:12 +0900

    feat(ui): 모바일 사이드바 서브메뉴 구현

--- components/global-header.tsx
+++ components/global-header.tsx
@@ -31,7 +31,9 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { group } from "node:console";
+import { useState } from "react";
+import { ArrowRightIcon } from "@/resources/icons/ArrowRight";
+import { ArrowLeftIcon } from "@/resources/icons/ArrowLeft";
 
 type NavMenu =
   | {
@@ -109,7 +111,7 @@ function groupSubMenu(
   return result;
 }
 
-export function GlobalHeader() {
+function DesktopNavMenu({ menus }: { menus: Array<NavMenu> }) {
   const pathname = usePathname();
 
   const generateDropdownContent = (submenu: Array<NavMenu>) =>
@@ -138,78 +140,130 @@ export function GlobalHeader() {
     );
 
   return (
-    <div className="w-full flex justify-center items-center bg-white border-b border-border">
-      <div className="flex gap-6 justify-between items-center py-5 px-4 md:px-12 w-full max-w-7xl">
-        <div className="flex items-center justify-center gap-2">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="ghost" size="icon-lg" className="flex md:hidden">
-                <MenuIcon className="size-5" />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" showCloseButton={false}>
-              <SheetHeader>
-                <SheetTitle asChild>
-                  <div className="w-[100px]">
-                    <img src="/images/belchic_logo.png" alt="belchic logo" />
-                  </div>
-                </SheetTitle>
-                <SheetDescription />
-              </SheetHeader>
-              <div className="flex-1 flex flex-col">
-                {NAV_MENU.map(
-                  (item) =>
-                    "path" in item && (
-                      <Link
-                        key={item.label}
-                        className={cn(
-                          "py-2 px-6 group",
-                          pathname === item.path && "bg-secondary",
-                        )}
-                        href={item.path}
-                      >
-                        <span className="text-lg">{item.label}</span>
-                      </Link>
-                    ),
-                )}
-              </div>
-            </SheetContent>
-          </Sheet>
-          <div className="w-[100px]">
-            <img src="/images/belchic_logo.png" alt="belchic logo" />
-          </div>
-        </div>
-        <div className="flex-1 hidden md:flex flex-wrap">
-          {NAV_MENU.map((item) =>
-            "path" in item ? (
-              <Link key={item.label} className="p-2 group" href={item.path}>
-                <span
-                  className={cn(
-                    "group-hover:border-b border-border",
-                    pathname === item.path &&
-                      "border-b group-hover:border-black",
-                  )}
-                >
+    <div className="flex-1 hidden md:flex flex-wrap">
+      {menus.map((item, idx) =>
+        "path" in item ? (
+          <Link key={idx} className="p-2 group" href={item.path}>
+            <span
+              className={cn(
+                "group-hover:border-b border-border",
+                pathname === item.path && "border-b group-hover:border-black",
+              )}
+            >
+              {item.label}
+            </span>
+          </Link>
+        ) : (
+          <DropdownMenu key={item.label}>
+            <DropdownMenuTrigger asChild>
+              <div className="p-2 flex items-center cursor-pointer group">
+                <span className={cn("group-hover:border-b border-border")}>
                   {item.label}
                 </span>
+                <ChevronDownIcon strokeWidth={1} className="size-5" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="rounded-none **:rounded-none">
+              {generateDropdownContent(item.submenu)}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ),
+      )}
+    </div>
+  );
+}
+
+function MobileNavSheet({
+  menus,
+  subtitle,
+  Trigger,
+}: {
+  menus: Array<NavMenu>;
+  subtitle?: string;
+  Trigger?: () => React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  const [sheetOpen, setSheetOpen] = useState(false);
+
+  return (
+    <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+      {Trigger ? (
+        <Trigger />
+      ) : (
+        <SheetTrigger asChild>
+          <Button variant="ghost" size="icon-lg" className="flex md:hidden">
+            <MenuIcon className="size-5" />
+          </Button>
+        </SheetTrigger>
+      )}
+      <SheetContent side="left" showCloseButton={false}>
+        <SheetHeader>
+          <SheetTitle asChild>
+            <div className="w-[100px]">
+              <img src="/images/belchic_logo.png" alt="belchic logo" />
+            </div>
+          </SheetTitle>
+          <SheetDescription />
+        </SheetHeader>
+        <div className="flex flex-col h-full w-full overflow-y-auto">
+          {subtitle && (
+            <button
+              className="py-3 px-4.5 flex items-center gap-2"
+              onClick={() => {
+                setSheetOpen(false);
+              }}
+            >
+              <ArrowLeftIcon className="size-4" />
+              <span className="text-sm">{subtitle}</span>
+            </button>
+          )}
+          {menus.map((item, idx) =>
+            "path" in item ? (
+              <Link
+                key={idx}
+                className={cn(
+                  "py-2 px-6",
+                  pathname === item.path && "bg-secondary",
+                )}
+                href={item.path}
+                onClick={() => {
+                  setSheetOpen(false);
+                }}
+              >
+                <span className="text-lg">{item.label}</span>
               </Link>
             ) : (
-              <DropdownMenu key={item.label}>
-                <DropdownMenuTrigger asChild>
-                  <div className="p-2 flex items-center cursor-pointer group">
-                    <span className={cn("group-hover:border-b border-border")}>
-                      {item.label}
-                    </span>
-                    <ChevronDownIcon strokeWidth={1} className="size-5" />
-                  </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="rounded-none **:rounded-none">
-                  {generateDropdownContent(item.submenu)}
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <MobileNavSheet
+                key={idx}
+                menus={item.submenu}
+                subtitle={item.label}
+                Trigger={() => (
+                  <SheetTrigger className="py-2 px-6 w-full flex justify-between items-center">
+                    <span className="text-lg">{item.label}</span>
+                    <ArrowRightIcon className="size-4" />
+                  </SheetTrigger>
+                )}
+              />
             ),
           )}
         </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export function GlobalHeader() {
+  return (
+    <div className="w-full flex justify-center items-center bg-white border-b border-border">
+      <div className="flex gap-6 justify-between items-center py-5 px-4 md:px-12 w-full max-w-7xl">
+        <div className="flex items-center justify-center gap-2">
+          <MobileNavSheet menus={NAV_MENU} />
+          <Link href="/" className="w-[100px]">
+            <img src="/images/belchic_logo.png" alt="belchic logo" />
+          </Link>
+        </div>
+        <DesktopNavMenu menus={NAV_MENU} />
         <div className="flex items-center justify-center">
           <Button variant="ghost" size="icon-lg">
             <SearchIcon className="size-5" />
