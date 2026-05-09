d88261cc7f1f88257615c9276b71ae6e2fb98daf
Seoa Yoon <me@seoa.dev>
2026-05-09 12:55:43 +0900

    feat(font): Outfit font weight 300 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const pretendard = localFont({
 
 const outfit = Outfit({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-outfit",
 });
 
