5683f57e23b70bc9f29c554a073b6ff0a367ee23
Seoa Yoon <bananamilk452@gmail.com>
2026-05-10 14:50:08 +0900

    feat(action): action 및 query가 ActionResult 타입으로 return하게 변경, 핸들링

--- lib/queries/product.query.ts
+++ lib/queries/product.query.ts
@@ -5,23 +5,24 @@ import {
   getProductsAction,
   getRelatedProductsByHandleAction,
 } from "../actions/product.action";
+import { wrapQueryFn } from "../utils";
 
 import type { GetProductsParams } from "../models/product.model";
 
 export const productsQueryOptions = (params: GetProductsParams = { page: 1, limit: 10 }) =>
   queryOptions({
     queryKey: ["products", params],
-    queryFn: () => getProductsAction(params),
+    queryFn: wrapQueryFn(() => getProductsAction(params)),
   });
 
 export const productByHandleQueryOptions = (handle: string) =>
   queryOptions({
     queryKey: ["product", handle],
-    queryFn: () => getProductByHandleAction(handle),
+    queryFn: wrapQueryFn(() => getProductByHandleAction(handle)),
   });
 
 export const relatedProductsByHandleQueryOptions = (handle: string, limit = 10) =>
   queryOptions({
     queryKey: ["relatedProducts", handle, limit],
-    queryFn: () => getRelatedProductsByHandleAction({ handle, limit }),
+    queryFn: wrapQueryFn(() => getRelatedProductsByHandleAction({ handle, limit })),
   });
